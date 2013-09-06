b1e1407522819238120520a9d86dccad449189f3
edwardlando <edwardlando8@gmail.com>
2013-09-06 13:45:48 -0400

    app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffeeshop::Application.config.secret_token = 'a2addc429a4c23e2eee66e2711bcd0df42208e0b901f97aa2a477b1b6804db692c08c39ca441969150763a24a25ad919d3d639cfec9ec948b575171d6fef73e5'
